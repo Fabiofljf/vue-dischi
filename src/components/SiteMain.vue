8b49997a5b9e9b0da6d091132970128769389789
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-05 17:20:30 +0200

    push

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,11 +1,25 @@
 <template>
-  <div id="downWindows">
-    <div class="container">
-      <div class="row row-cols-5">
-        <div class="col" v-for="(card, index) in cards" :key="index">
-          <DiskCard :img="card.poster" :title="card.title" :author="card.author" :year="card.year" />
+  <div>
+    <div id="downWindows" v-if="!loading">
+      <div class="container">
+        <div class="row row-cols-5">
+          <div class="col" v-for="(card, index) in cards" :key="index">
+            <DiskCard
+              :img="card.poster"
+              :title="card.title"
+              :author="card.author"
+              :year="card.year"
+            />
+          </div>
         </div>
       </div>
+    </div>
+    <!-- /downWindows -->
+    <div
+      class="d-flex min-vh-100 align-items-center justify-content-center"
+      v-else
+    >
+      <h1>loading ...</h1>
     </div>
   </div>
 </template>
@@ -23,22 +37,29 @@ export default {
     return {
       API_link: "https://flynn.boolean.careers/exercises/api/array/music",
       cards: null,
+      loading: true,
     };
   },
   methods: {
-      CallApi(){
-          axios
-          .get(this.API_link)
-          .then((response)=>{
-              //console.log(response);
-              this.cards = response.data.response //Array dischi  
-              //console.log(response.data.response);        
-          })
-      }
+    CallApi() {
+      axios
+        .get(this.API_link)
+        .then((response) => {
+          //console.log(response);
+          this.cards = response.data.response; //Array dischi
+          //console.log(response.data.response);
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error();
+          error;
+          this.error = `Sorry There is a problem! ${error}`;
+        });
+    },
   },
-  mounted(){
-      this.CallApi()
-  }
+  mounted() {
+    this.CallApi();
+  },
 };
 </script>
 
